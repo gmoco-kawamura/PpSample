7a7ede54842f3d6173766fa780b60f39c3d19f4c
gmoco-kawamura <158865565+gmoco-kawamura@users.noreply.github.com>
2024-02-28 12:24:20 +0900

    Initial Commit

---
+++ PpSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PpSample
+//
+//  Created by 川村拓也 on 2024/02/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PpSample/PpSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PpSampleApp.swift
+//  PpSample
+//
+//  Created by 川村拓也 on 2024/02/28.
+//
+
+import SwiftUI
+
+@main
+struct PpSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
